057a92b91494527884be361fa27669068d69d337
Ernesto224 <ev402648@gmail.com>
2025-10-08 23:59:27 -0500

    mejoras de documentacion swagger

--- src/config/swaggerConfig.ts
+++ src/config/swaggerConfig.ts
@@ -2,24 +2,24 @@ import swaggerJSDoc from 'swagger-jsdoc';
 
 const swaggerOptions = {
   definition: {
-    openapi: '3.1.1',
+    openapi: '3.0.4',
     info: {
-      title: 'God-Of-War-Weapon-API',
+      title: 'God of War Weapons API',
       version: '1.0.0',
       description: 'API for managing God of War games and weapons information',
+      contact: {
+        name: 'API Support',
+        url: 'https://github.com/Ernesto224/God-Of-War-Weapon-API'
+      },
     },
     servers: [
       {
-        url: process.env.VERCEL_URL 
-          ? `https://${process.env.VERCEL_URL}/api`
-          : 'http://localhost:3001/api',
-        description: process.env.NODE_ENV === 'production' 
-          ? 'Production server' 
-          : 'Development server',
+        url: 'http://localhost:3001/api',
+        description: 'Development server'
       }
     ],
   },
-  apis: ['src/types/swaggerSchemas.ts','src/routes/*.ts'],
+  apis: ['./swagger.yaml'],
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
