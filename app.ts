ac6766c1d243ec4b2c85103d8880dda8cb809609
Ernesto224 <ev402648@gmail.com>
2025-10-12 01:05:27 -0500

    ajustes para despliegue de swagger

--- app.ts
+++ app.ts
@@ -6,7 +6,6 @@ import swaggerSpec from './src/config/swaggerConfig';
 import routes from './src/routes/index.routes';
 import errorHandler from './src/middleware/errorHandler';
 import notFoundHandler from './src/middleware/notFoundHandler';
-import { ApplicationResponse } from './src/types/types';
 
 /**
  * Configure middleware for the Express application
@@ -58,13 +57,6 @@ const initializeApp = (): Application => {
     const app: Application = express();
 
     configureMiddleware(app);
-    app.use('/', (req, res) => {
-    var response: ApplicationResponse = {
-        success: true,
-        message: 'Welcome to the God of War Weapons API'
-    };
-    return res.status(200).json(response);
-});
     configureSwagger(app);
     configureRoutes(app);
     configureErrorHandling(app);
